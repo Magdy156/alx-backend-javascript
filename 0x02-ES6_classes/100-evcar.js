40bedf88ae1a40f3fee8ef4c24f5f0fb8bf368c1
Magdy156 <magdyhamdy156@gmail.com>
2024-03-13 00:06:23 +0200

    task11

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,22 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
