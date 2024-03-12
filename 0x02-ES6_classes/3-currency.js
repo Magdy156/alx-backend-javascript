38f11a06c5dcbc9e7e75493127d1dca87efb4136
Magdy156 <magdyhamdy156@gmail.com>
2024-03-12 21:26:23 +0200

    task3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Length must be a string');
+    }
+    this._code = newCode;
+  }
+
+  get code() {
+    return this._length;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
